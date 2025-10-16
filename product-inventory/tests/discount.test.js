fa5be794d87a7709623b76ed149326c2c0034f00
hursha <hurshaparveen@gmail.com>
2025-10-16 18:51:50 -0500

    added more test cases

--- product-inventory/tests/discount.test.js
+++ product-inventory/tests/discount.test.js
@@ -6,6 +6,13 @@ describe("calculate discount tests ", () => {
   test("calculate discount with valid price and discount ", () => {
     expect(calculateDiscount(100, 0.1)).toBe(90);
   });
-  
-});
+  // negative test
+  test("calculate discount with invalid discount rate ", () => {
+    expect(calculateDiscount(100, -0.1)).toBe(null);
+  });
 
+  // edge case
+  test("calculate discount with invalid price ", () => {
+    expect(calculateDiscount(0, 0.1)).toBe(0);
+  });
+});
